09e524f223700ca6fa6f38d667c2f66e6a7218cd
abkathiriya <akashk@arista.com>
2017-02-02 19:29:53 +0530

    minor changes

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/connections")
+def connection():
+	return render_template("connections.html")
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
